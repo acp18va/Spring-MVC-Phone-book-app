b262748fc43a389480206c5eca3ff79ddea8f338
Vijeta Agrawal <vijetaagrawal@Vijetas-MacBook-Pro.local>
2019-04-08 08:40:24 +0100

    new configs

---
+++ springcontactapp/src/main/java/com/capp/config/SpringRootConfig.java
@@ -0,0 +1,11 @@
+package com.capp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com")
+
+public class SpringRootConfig {
+//TODO: Dao, Services, DataSources Email Sender, Business layer Beans 
+}
